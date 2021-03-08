397983b10b5ea755c6b4511a232d0f9694eb7c15
Artur Igberdin <art.igberdin@gmail.com>
2021-03-08 17:27:13 +0300

    Add PlaceViewController with choosing arrival and departure city/airport

---
+++ avia-tickets/Modules/Place/PlaceViewController.h
@@ -0,0 +1,29 @@
+//
+//  PlaceViewController.h
+//  avia-tickets
+//
+//  Created by Artur Igberdin on 05.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+#import "DataManager.h"
+
+typedef enum PlaceType {
+    //Прибытие
+    PlaceTypeArrival,
+    //Отправление
+    PlaceTypeDeparture
+} PlaceType;
+
+@protocol PlaceViewControllerDelegate <NSObject>
+- (void)selectPlace:(id)place withType:(PlaceType)placeType andDataType:(DataSourceType)dataType;
+@end
+
+@interface PlaceViewController : UITableViewController
+
+@property (nonatomic, strong) id<PlaceViewControllerDelegate>delegate;
+
+- (instancetype)initWithType:(PlaceType)type;
+
+@end
+
